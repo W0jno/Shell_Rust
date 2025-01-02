4fc31b44a778e55005ddb338484e3cc6666302b0
w0jno <filip.wojno03@o2.pl>
2025-01-02 18:51:52 +0100

    pipes, mkdir, touch, rmdir, rm

--- shell_rust/src/main.rs
+++ shell_rust/src/main.rs
@@ -3,7 +3,8 @@ use std::io::{stdin,stdout,Write};
 use std::{ env, fs};
 use std::process::exit;
 use glob::glob_with;
- use std::path::PathBuf;
+use std::path::PathBuf;
+use std::fs::File;
 use colored::Colorize;
 
 /* enum KeyWords {
@@ -22,16 +23,17 @@ struct Command {
 fn main() {
     loop{
         let mut input = String::new();
-        //let mut vecInput: Vec<String> = Vec::new();
         print!("> ");
         stdout().flush().expect("Failed to flush stdout");
         stdin().read_line(&mut input).expect("Did not enter correct string");
-        let mut vec_input: Vec<String> = input.split_whitespace().map(String::from).collect();
-        let command = Command {
-            keyword: vec_input.remove(0),
-            arguments: vec_input.clone(),
-        };
-        execute_command(command)
+        let input_commads: Vec<String> = input.trim().split('|').map(String::from).collect();
+        for command_str in input_commads {
+            let mut parts = command_str.trim().split_whitespace();
+            let keyword = parts.next().unwrap_or("").to_string();
+            let arguments = parts.map(String::from).collect();
+            let command = Command { keyword, arguments };
+            execute_command(command);
+        }
     }
 }
 
@@ -50,6 +52,26 @@ fn execute_command(command: Command) {
         "cd" => {
             if let Err(e) = execute_cd(command.arguments) {
                 eprintln!("Error executing cd: {}", e);
+            }
+        },
+        "mkdir" => {
+            if let Err(e) = execute_mkdir(command.arguments) {
+                eprintln!("Error executing mkdir: {}", e);
+            }
+        },
+        "touch" => {
+            if let Err(e) = execute_touch(command.arguments) {
+                eprintln!("Error executing touch: {}", e);
+            }
+        },
+        "rmdir" => {
+            if let Err(e) = execute_rmdir(command.arguments) {
+                eprintln!("Error executing rmdir: {}", e);
+            }
+        },
+        "rm" => {
+            if let Err(e) = execute_rm(command.arguments) {
+                eprintln!("Error executing rm: {}", e);
             }
         },
         "exit" => {
@@ -95,5 +117,54 @@ fn execute_cd(arguments: Vec<String>) -> Result<(), Box<dyn std::error::Error>>{
         next_path.push(&arguments[0]);
         env::set_current_dir(next_path)?;
     }
+    Ok(())
+}
+
+fn execute_mkdir(arguments: Vec<String>) -> Result<(), Box<dyn std::error::Error>>{
+     let current_path = env::current_dir()?;
+     if arguments.is_empty(){
+        eprintln!("Provide directory name");
+     }
+     let mut dir_path = current_path;
+     dir_path.push(&arguments[0]);
+     fs::create_dir(dir_path)?;
+     Ok(())
+}
+
+fn execute_touch(arguments: Vec<String>) -> Result<(), Box<dyn std::error::Error>>{
+     let current_path = env::current_dir()?;
+     if arguments.is_empty(){
+        eprintln!("Provide file name");
+     }
+     let mut file_path = current_path;
+     file_path.push(&arguments[0]);
+     File::create(file_path);
+     Ok(())
+}
+
+fn execute_rmdir(arguments: Vec<String>) -> Result<(), Box<dyn std::error::Error>>{
+    let current_path = env::current_dir()?;
+    if arguments.is_empty() {
+        eprintln!("Provide directory name");
+    } 
+    let mut dir_path = current_path;
+    dir_path.push(&arguments[0]);
+    let entries = fs::read_dir(&dir_path)?;
+    if entries.count() == 0 {
+            fs::remove_dir(dir_path)?;
+        } else {
+            eprintln!("Directory is not empty!");
+        }
+    Ok(())
+}
+
+fn execute_rm(arguments: Vec<String>) -> Result<(), Box<dyn std::error::Error>>{
+    let current_path = env::current_dir()?;
+    if arguments.is_empty() {
+        eprintln!("Provide file name");
+    } 
+    let mut file_path = current_path;
+    file_path.push(&arguments[0]);
+    fs::remove_file(file_path)?;
     Ok(())
 }
